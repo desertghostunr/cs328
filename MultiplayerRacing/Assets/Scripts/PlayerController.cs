0f23893adcaf07730c8c45ce77861e61969e4e58
Tyler <tylergoffinet@cox.net>
2017-04-15 02:50:33 -0700

    Add player movement for WASD
    Implemented AddTorque and AddForce for Kayak movement along with drag to
    add realism and playability.
    
    During implementation, I couldn't get AddForce(transform.foward) to work?
    Resorted to using AddForce(transform.right) which is counter intuitive.
    Then again, maybe I'm thinking about it wrong.

---
+++ MultiplayerRacing/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody))]
+
+public class PlayerController : MonoBehaviour
+{
+    // UI Input
+    public float speedMuliplier = 10.0f;
+    public float rotateMulitpler = 10.0f;
+    public float maxTurnAngle = 30.0f;
+
+    // Input
+    private float horiAxis, vertAxis;
+
+    // Physics
+    private Rigidbody rigid;
+
+	void Start()
+    {
+        rigid = GetComponent<Rigidbody>();
+	}
+	
+	void Update()
+    {
+        // Take input
+        horiAxis = Input.GetAxis("Horizontal");
+        vertAxis = Input.GetAxis("Vertical");
+	}
+
+    void FixedUpdate()
+    {
+        rigid.AddForce(transform.right * vertAxis * speedMuliplier);
+        rigid.AddTorque(transform.up * horiAxis * rotateMulitpler);
+    }
+}
