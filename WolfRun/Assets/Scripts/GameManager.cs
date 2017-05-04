09d0bd830aadb1b7a780c29a04c0b8b1f47032b2
Tyler <tylergoffinet@cox.net>
2017-05-04 00:42:54 -0700

    Add in-game pause menu
    Panel with white buttons that don't do anything YET

---
+++ WolfRun/Assets/Scripts/GameManager.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    // Public variables
+
+
+    // Private variables
+    bool isPaused = false;
+    GameObject pausePanel;
+
+    private void Start()
+    {
+        pausePanel = GameObject.Find("Pause Panel");
+        pausePanel.SetActive(false);
+    }
+
+    private void Update ()
+    {
+        if (Input.GetButtonDown("Cancel"))
+        {
+            TogglePauseState();
+        }
+	}    
+
+    public void TogglePauseState()
+    {
+        if (isPaused)
+        {
+            // Unpause game
+            Time.timeScale = 1;
+            pausePanel.SetActive(false);
+            isPaused = false;
+        }
+        else
+        {
+            // Pause game
+            Time.timeScale = 0;
+            pausePanel.SetActive(true);
+            isPaused = true;
+        }
+    }
+}
+
