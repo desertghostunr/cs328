7b7b865d633f89c45929196d3604a1ea8e16164b
Tyler <tylergoffinet@cox.net>
2017-05-03 21:02:10 -0700

    Add main menu and other related assets
    Creates 'generic button' prefab for animated button and lays out main
    menu.

---
+++ WolfRun/Assets/Scripts/ButtonController.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine.EventSystems;
+using UnityEngine.UI;
+using UnityEngine;
+
+public class ButtonController : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
+{
+    public string threatText;
+    public Color threatColor;
+
+    private Text childText;
+    private string defaultText;
+    private Color defaultColor;
+
+    private void Start()
+    {
+        childText = GetComponentInChildren<Text>();
+        defaultText = childText.text;
+        defaultColor = childText.color;
+    }
+
+    public void OnPointerEnter(PointerEventData pointer)
+    {
+        childText.text = threatText;
+        childText.color = threatColor;
+    }
+
+    public void OnPointerExit(PointerEventData pointer)
+    {
+        childText.text = defaultText;
+        childText.color = defaultColor;
+    }
+}
