94ed0a37b94c5ddf4e739ecf571601d31570f4b6
Tyler <tylergoffinet@cox.net>
2017-05-03 21:58:01 -0700

    Add instructions to menu
    Create new UI buttons and text to implement instructions menu. Link
    between 'preparation' button and showing instruction menu DOES NOT WORK
    and don't know why... :(

---
+++ WolfRun/Assets/Scripts/MenuManager.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+using UnityEngine;
+
+public class MenuManager : MonoBehaviour
+{
+    public void PlayGame()
+    {
+        SceneManager.LoadScene("scene1");
+    }
+
+    public void GotoHowToScreen()
+    {
+        ShowMainScreen(false);
+        ShowHowToScreen(true);
+    }
+
+    public void GotoMainScreen()
+    {
+        ShowHowToScreen(false);
+        ShowMainScreen(true);
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+
+    private void ShowMainScreen(bool shouldShow)
+    {
+        foreach (GameObject mainObj in GameObject.FindGameObjectsWithTag("MainScreen"))
+        {
+            mainObj.SetActive(shouldShow);
+        }
+    }
+
+    private void ShowHowToScreen(bool shouldShow)
+    {
+        foreach (GameObject howObj in GameObject.FindGameObjectsWithTag("HowToScreen"))
+        {
+            howObj.SetActive(shouldShow);
+        }
+    }
+}
