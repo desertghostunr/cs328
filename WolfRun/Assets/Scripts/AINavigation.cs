327698c127941212ae4164548f7f69c43f59fbfa
desertghostunr <desertghost@nevada.unr.edu>
2017-05-22 13:48:37 -0700

    Improved nav obstacle detection.

--- WolfRun/Assets/Scripts/AINavigation.cs
+++ WolfRun/Assets/Scripts/AINavigation.cs
@@ -20,6 +20,9 @@ public class AINavigation : MonoBehaviour
     private TerrainMotionController m_terrainMotionControl;
     private GameObject m_currentTerrainGO = null;
 
+
+    private Collider[] m_obstacles;
+
     private bool m_navMapBuilt = false;
     private bool m_destinationReached = false;
     private bool m_routePlanned = false;
@@ -27,6 +30,8 @@ public class AINavigation : MonoBehaviour
 
     private float[,] m_navigationMap;
     private float[,] m_tmpNavigationMap;
+
+    private Vector3 m_navMapOffsetPositon;
 
     private Vector3 m_destination;
 
@@ -62,6 +67,7 @@ public class AINavigation : MonoBehaviour
 		if( !m_navMapBuilt )
         {
             //build nav map
+            m_navMapOffsetPositon = transform.position;
             m_buildNavMapThread.Start( );            
         }
         else if ( m_navMapBuilt && !m_routePlanned )
@@ -96,15 +102,13 @@ public class AINavigation : MonoBehaviour
         Vector3 position;
         int x, y;
 
-        m_navMapBuilt = false;
+        m_navMapBuilt = false;        
 
         for( y = 0; y < m_tmpNavigationMap.GetLength( 0 ); y++ )
         {
             for ( x = 0; x < m_tmpNavigationMap.GetLength( 1 ); x++ )
             {
-                position.x = x;
-                position.y = 0;
-                position.z = y;
+                position = GetWorldPositionFromNavMapCoords( x, y );
 
                 m_tmpNavigationMap[y, x] 
                     = m_terrainInfo.getTerrainWeight( position, 
@@ -119,18 +123,38 @@ public class AINavigation : MonoBehaviour
         m_navMapBuilt = true;
     }
 
+    public int GetNavMapX( Vector3 worldPosition )
+    {
+        return ( int ) ( worldPosition.x - m_navMapOffsetPositon.x + ( mapDim / 2.0f ) );
+    }
+
+    public int GetNavMapY( Vector3 worldPosition )
+    {
+        return ( int ) ( worldPosition.z - m_navMapOffsetPositon.z + ( mapDim / 2.0f ) );
+    }
+
+    public Vector3 GetWorldPositionFromNavMapCoords( int x, int y )
+    {
+        Vector3 position;
+
+        position.x = x + m_navMapOffsetPositon.x - ( mapDim / 2.0f );
+        position.y = 0 + m_navMapOffsetPositon.y;
+        position.z = y + m_navMapOffsetPositon.z - ( mapDim / 2.0f );
+
+        return position;
+    }
+
     public void AddObstaclesToNavMap( )
     {
         int index;
-        Collider[] obstacles;
 
         StartCoroutine( RefreshTimer( ) );
 
-        obstacles = Physics.OverlapSphere( transform.position, mapDim );
+        m_obstacles = Physics.OverlapSphere( transform.position, mapDim );
 
-        for( index = 0; index < obstacles.Length; index++ )
+        for( index = 0; index < m_obstacles.Length; index++ )
         {
-            SetMapWeight( ref m_navigationMap, obstacles[index].transform.position, 0 );
+            SetObstacleMapWeight( ref m_navigationMap, m_obstacles[index], 0 );
         }
     }
 
@@ -153,18 +177,48 @@ public class AINavigation : MonoBehaviour
         m_refreshObstacles = true;
     }
 
-    public static bool SetMapWeight( ref float[,] map, Vector3 position, float weight )
+    public void SetObstacleMapWeight( ref float[,] map, Collider obstacle, float weight )
     {
-        if( (int) position.x < 0 || (int) position.z < 0 
-            || (int) position.x >= map.GetLength( 1 ) 
-            || (int) position.z >= map.GetLength( 0 ) )
+        Vector3 position = obstacle.transform.position;
+        int x, y;
+        int index;
+        List<Vector3> positionBlock = GetDirectNeighborhood( position );
+
+        for( index = 0; index < positionBlock.Count; index++ )
         {
-            return false;
+            if ( obstacle.bounds.Contains( positionBlock[index] ) )
+            {
+                x = GetNavMapX( positionBlock[index] );
+                y = GetNavMapY( positionBlock[index] );
+
+                if ( x < 0
+                     || y < 0
+                     || x >= map.GetLength( 1 )
+                     || y >= map.GetLength( 0 ) )
+                {
+                    continue;
+                }
+
+                map[y, x] = weight;
+            }
+            
+        }
+    }
+
+    public List<Vector3> GetDirectNeighborhood( Vector3 position )
+    {
+        List<Vector3> nList = new List<Vector3>();
+        int x, y;
+
+        for( y = -1; y <= 1; y++ )
+        {
+            for( x = -1; x <= 1; x++ )
+            {
+                nList.Add( new Vector3( position.x + x, position.y, position.z + y ) );
+            }
         }
 
-        map[( int ) position.z, ( int ) position.x] = weight;
-
-        return true;
+        return nList;
     }
 
     private void OnCollisionEnter( Collision collision )
